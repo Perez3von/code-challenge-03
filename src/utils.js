d0f90e5f15f40c156252d7b0e9b0f9e75a5b4f5a
Evon Perez <perez.evon@gmail.com>
2021-08-20 09:42:09 -0400

    adding code challenge 04

--- src/utils.js
+++ src/utils.js
@@ -36,3 +36,64 @@ export function sortedArraysByValuesLength(obj) {
 return main_arr.sort((a, b) => b[1].length - a[1].length);
 
 }
+
+
+export function getHouses(arr) {
+    const res = arr.sort((a, b)=>{
+        return a.children.length - b.children.length;
+    });
+
+    return res.map(obj=>obj.house);
+}
+
+export function updateNumbers(obj) {
+    let arr =[]
+    for (const [key, val] of Object.entries(obj)) {
+        arr.push(`${key}: ${val}`);
+      }
+return arr;
+      
+}
+
+
+export function totalCharacters(arr) {
+
+let total =0;
+
+arr.map((obj) => {
+
+    if(obj.spouse !== null){
+
+        total = total + 2 + obj.children.length;  
+    }
+    else{
+        total = total+ 1 + obj.children.length;
+    }
+    return 0;
+     
+})
+
+return total;
+
+}
+
+
+
+
+export function hasChildrenEntries(arr, character) {
+    for( let [key, value] of Object.entries(arr)){
+       if( value.name === character && value.children !== []){
+           return true;
+       } else {
+           return false;
+       }
+    }
+ } 
+ 
+
+ export function sortByChildren(arr){
+    return arr.sort((a, b)=>{
+          return a.children.length - b.children.length;
+      })
+      
+     }
